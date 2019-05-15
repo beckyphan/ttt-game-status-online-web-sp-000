f55b942f16dc1e2a77599f5b8297c45cfd0ea0f0
Becky Phan <beckyphan@live.com>
2019-05-15 03:52:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,11 +30,7 @@ def won?(board)
     end
   end
 
-  WIN_COMBINATIONS.each do |winning_combo|
-    if winning_combo == player_indices
-      return player_indices
-    else
-      return false
-    end
+  WIN_COMBINATIONS.select do |winning_combo|
+    winning_combo == player_indices
   end
 end
